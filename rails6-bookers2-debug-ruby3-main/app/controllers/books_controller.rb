3506659811c2222a8bd3a7b54e88f168a600edbf
YASU0210 <150024274+YASU0210@users.noreply.github.com>
2024-01-13 15:42:29 +0000

    db2

--- rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
-    @books = Book.all
+    @books = Book.new
   end
 
   def create
@@ -20,7 +23,7 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
@@ -32,15 +35,23 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
-    redirect_to books_path
+    @book.destroy
+    redirect_to books_path, notice: "successfully delete book!"
   end
 
   private
-
+  
+  def ensure_correct_user
+    @book = Book.find(params[:id])
+      unless @book.user == current_user
+        redirect_to books_pathw
+      end
+  end
+    
+    
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
